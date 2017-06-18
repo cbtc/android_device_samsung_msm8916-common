a093d2cc2d29de0fc5affc67af09b0bea174a032
cbtc <carlos_ddg@yahoo.es>
2017-06-18 22:27:26 +0200

    uploading base

---
+++ product/qcom-audio.mk
@@ -0,0 +1,24 @@
+# Audio
+PRODUCT_PACKAGES += \
+	audio.a2dp.default \
+	audio.primary.msm8916 \
+	audio.primary.default \
+	audio_policy.msm8916 \
+	audio.r_submix.default \
+	audio.tms.default \
+	audio.usb.default \
+	audiod \
+	libaudio-resampler \
+	libaudioroute \
+	libaudioutils \
+	libaudiopolicymanager \
+	libqcompostprocbundle \
+	libqcomvisualizer \
+	libqcomvoiceprocessing \
+	libqcmediaplayer \
+	libtinyalsa \
+	libtinycompress \
+	tinymix \
+	tinyplay \
+	tinycap \
+	tinypcminfo
